9a2f4a0f2e29e19820781f1ca524a066179783f2
ljh97820 <ljh97820@naver.com>
2021-09-22 19:04:44 +0900

    fetchUser 수정 중

--- InstagramFirestoreTutorial/Controller/SearchController.swift
+++ InstagramFirestoreTutorial/Controller/SearchController.swift
@@ -7,22 +7,49 @@
 
 import UIKit
 
-class SearchController: UIViewController {
+private let reuseIdentifier = "UserCell"
+
+class SearchController: UITableViewController {
+    
+    // MARK: Lifecycle
+    
+    private var users = [User]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureTableView()
+        fetchUsers()
+    }
+    
+    // MARK: API
+    
+    func fetchUsers() {
+        UserService.fetchUsers { users in
+            self.users = users
+            self.tableView.reloadData() // 열의 카운트를 불러와야하기 때문에 reloadData가 필요함
+        }
     }
     
     func configureTableView() {
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .white
         
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
     }
 }
 
-// MARK: TableView DataSource
+// MARK: UITableViewDataSource
 
 extension SearchController {
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        cell.user = users[indexPath.row]
+        return cell
+    }
 }
