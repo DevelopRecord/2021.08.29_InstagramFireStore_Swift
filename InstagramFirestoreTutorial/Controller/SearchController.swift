95ce73be0f765f485f31b78f5ede176330449514
ljh97820 <ljh97820@naver.com>
2021-09-25 18:20:00 +0900

    SearchController 검색기능 활성화

--- InstagramFirestoreTutorial/Controller/SearchController.swift
+++ InstagramFirestoreTutorial/Controller/SearchController.swift
@@ -14,10 +14,16 @@ class SearchController: UITableViewController {
     // MARK: Lifecycle
     
     private var users = [User]()
+    private var filteredUser = [User]()
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private var inSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureSearchController()
         configureTableView()
         fetchUsers()
     }
@@ -31,11 +37,22 @@ class SearchController: UITableViewController {
         }
     }
     
+    // MARK: Helpers
+    
     func configureTableView() {
         view.backgroundColor = .white
         
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 64
+    }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
     }
 }
 
@@ -44,12 +61,41 @@ class SearchController: UITableViewController {
 extension SearchController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return inSearchMode ? filteredUser.count : users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        cell.user = users[indexPath.row]
+        
+        let user = inSearchMode ? filteredUser[indexPath.row] : users[indexPath.row]
+        cell.viewModel = UserCellViewModel(user: user)
         return cell
     }
+}
+
+// MARK: UITableViewDelegate
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = ProfileController(user: users[indexPath.row])
+        navigationController?.pushViewController(controller, animated: true)
+    }
+}
+
+// MARK: UISearchResultsUpdating
+
+extension SearchController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        
+        filteredUser = users.filter({
+            $0.username.contains(searchText) ||
+                $0.fullname.lowercased().contains(searchText)
+        })
+        
+        print("DEBUG: Filtered user \(filteredUser)")
+        self.tableView.reloadData()
+    }
+    
+    
 }
--- InstagramFirestoreTutorial/View/Profile/UserCell.swift
+++ InstagramFirestoreTutorial/View/Profile/UserCell.swift
@@ -6,16 +6,14 @@
 //
 
 import UIKit
+import SDWebImage
 
 class UserCell: UITableViewCell {
     
     // MARK: Properties
     
-    var user: User? {
-        didSet {
-            usernameLabel.text = user?.username
-            fullnameLabel.text = user?.fullname
-        }
+    var viewModel: UserCellViewModel? {
+        didSet { configure() }
     }
     
     private let profileImageView: UIImageView = {
@@ -64,5 +62,15 @@ class UserCell: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Helpers
+    
+    func configure() {
+        guard let viewModel = viewModel else { return }
+        
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        usernameLabel.text = viewModel.username
+        fullnameLabel.text = viewModel.fullname
     }
 }
