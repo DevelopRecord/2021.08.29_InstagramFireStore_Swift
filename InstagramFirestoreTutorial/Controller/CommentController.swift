5f3f455610e4ad98f42dcdb282557d82f158c29a
ljh97820 <ljh97820@naver.com>
2021-10-26 17:50:46 +0900

    CommentController 생성

---
+++ InstagramFirestoreTutorial/Controller/CommentController.swift
@@ -0,0 +1,55 @@
+//
+//  CommentController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by LeeJaeHyeok on 2021/10/26.
+//
+
+import UIKit
+
+private let reuseIdentifier = "CommentCell"
+
+class CommentController: UICollectionViewController {
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ConfigureCollectionView()
+    }
+    
+    // MARK: Helpers
+    
+    func ConfigureCollectionView() {
+        navigationItem.title = "Comments"
+        
+        collectionView.backgroundColor = .white
+        collectionView.register(CommentCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+}
+
+// MARK: UICollectionViewDataSource
+
+extension CommentController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+
+extension CommentController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.width, height: 80)
+    }
+}
---
+++ InstagramFirestoreTutorial/View/CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by LeeJaeHyeok on 2021/10/26.
+//
+
+import UIKit
+
+class CommentCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
