74acea525b86d5f4ca4d71f135ac988e7525a3c2
ljh97820 <ljh97820@naver.com>
2021-11-04 19:52:22 +0900

    좋아요(Like) 클릭 시 바로바로 collectionView reloadData

--- InstagramFirestoreTutorial/Controller/FeedController.swift
+++ InstagramFirestoreTutorial/Controller/FeedController.swift
@@ -14,7 +14,9 @@ class FeedController: UICollectionViewController {
     
     // MARK: Lifecycle
     
-    private var posts = [Post]()
+    private var posts = [Post]() {
+        didSet { collectionView.reloadData() }
+    }
     var post: Post?
     
     override func viewDidLoad() {
@@ -50,8 +52,18 @@ class FeedController: UICollectionViewController {
         
         PostService.fetchPosts { posts in
             self.posts = posts
+            self.checkIfUserLikedPosts()
             self.collectionView.refreshControl?.endRefreshing()
-            self.collectionView.reloadData()
+        }
+    }
+    
+    func checkIfUserLikedPosts() {
+        self.posts.forEach { post in
+            PostService.checkIfUserLikedPost(post: post) { didLike in
+                if let index = self.posts.firstIndex(where: { $0.postId == post.postId }) {
+                    self.posts[index].didLike = didLike
+                }
+            }
         }
     }
     
@@ -127,12 +139,14 @@ extension FeedController: FeedCellDelegate {
             PostService.UnLikePost(post: post) { _ in
                 cell.likeButton.setImage(#imageLiteral(resourceName: "like_unselected"), for: .normal)
                 cell.likeButton.tintColor = .black
+                cell.viewModel?.post.likes = post.likes - 1
             }
             
         } else {
             PostService.likePost(post: post) { _ in
                 cell.likeButton.setImage(#imageLiteral(resourceName: "like_selected"), for: .normal)
                 cell.likeButton.tintColor = .red
+                cell.viewModel?.post.likes = post.likes + 1
             }
         }
     }
